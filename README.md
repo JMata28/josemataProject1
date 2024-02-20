85633853f81e814e7c39daaa4392e969292154b7
José Mata <josemata@Joses-MacBook-Pro.local>
2024-02-20 15:38:47 -0500

    Fourth commit

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
 # josemataProject1
+
+This is Jose Amilcar Mata Calidonio's Project #1 for the COMP 510 course.
+
+The instructions for this assignment are shown below:
+
+Project 1 : fortune fun
+
+
+Summary: lets practice with files and go routines, making a multithreaded fortune program
+
+
+
+Due: Tuesday Feb 20th at 11:59pm (the day before our Monday on Wednesday class)
+
+This one will go into your project grade this time.
+
+Details:
+
+Create a new project. Make sure it has your name in it somewhere in the project name.
+
+
+(granted I am pushing the limits to try and get a simple program which also practices with channels and goroutines)
+
+
+Write go go program that has two functions
+
+
+
+The main function will
+
+1. create a channel and then spawn the fortune function as a go routine taking the channel as a parameter.
+
+2. Enter a forever loop and ask the user if they want another fortune. If the user says yes (with any capitalization) then send a message down the channel to the for the fortune function to select a fortune and display it.
+
+3. If the user answers no (with any capitalization), then end the program
+
+4. if the user answers anything else, then ask again
+
+
+The fortune function will
+
+1. Open the fortunes.txt file  and read it in.
+    1. be sure to use go:embed to embed the file into the final executable
+2. split the contents of the fortunes file on the %% string to get a slice of strings
+
+3. loop forever
+
+    1. wait for a message on the channel
+
+    2. when a message is received, randomly select one of the fortunes from your slice
+
+    3. and print it to the screen.
+
+
+Using goroutines and channels will feature heavily in this project’s grading
+
+
+Submitting the project.
+
+
+submit the project by putting it on github and sending me a collaboration invite. As before.
+
+the min/max opportunity of the month
--- josemataProject1.go
+++ josemataProject1.go
@@ -19,27 +19,48 @@ and store it in the string named "contentInAString"
 var contentInAString string
 
 func main() {
-	//channel := make(chan int)
-	//go fortune(channel)
-	var answer string //Create variable where answer of the user will be stored. It is initialized with a zero value here.
-	for {
-		fmt.Println("Would you like another fortune? Type yes or no") //Ask for user input
-		fmt.Scanln(&answer)                                           //Save user input in the variable "answer"
-		answerLowercase := strings.ToLower(answer)                    //Make "answer" all lowercase (this will ignore any capitalization in the user input)
+	channel1 := make(chan int)     //create channel named "channel1" that takes integers. This will be used to send a value that signals the fortune function to print out the fortune.
+	channel2 := make(chan int)     //create a channel named "channel2" that takes integers. This will be used to send a value that signals the UI that it can continue after the fortune function prints the fortune.
+	go fortune(channel1, channel2) //run the fortune function as a go routine and pass the channels as the parameters. Since it is a go routine, it will run "in the background" as its own lightweight thread.
+	var answer string              //Create variable where answer of the user will be stored. It is initialized with the zero value (an empty string) here.
+	for {                          //infinite for loop
+		fmt.Println("Would you like another fortune? Please type yes or no.") //Ask for user input
+		fmt.Scanln(&answer)                                                   //Save user input in the variable "answer"
+		answerLowercase := strings.ToLower(answer)                            //Make the string saved in variable "answer" all lowercase (this is used to ignore any capitalization in the user input in the if statements below)
 		if answerLowercase == "yes" {
-			fmt.Println("You answered yes.")
-			fortune()
-			//channel <- 1
+			channel1 <- 1          //Sends a value of 1 into the channel named "channel1"
+			message2 := <-channel2 //Wait until channel2 is loaded with data (which will only happen after the fortune is printed) and once it is, store it in the variable "message2"
+			if message2 == 1 {     //This is just an empty if statement so the variable "message2" is used.
+			}
 		} else if answerLowercase == "no" {
-			fmt.Println("You answered no. Program terminated.")
 			os.Exit(0) //This function terminates the program. The parameter 0 signals an exit with no error.
 		}
 	}
 }
 
-func fortune() {
-	contentInASlice := strings.Split(contentInAString, "%%") //Creates a slice of strings separated by the "%%" symbols
-	numberOfFortunes := len(contentInASlice)
-	randomnumber := rand.Intn(numberOfFortunes)
-	fmt.Println(contentInASlice[randomnumber])
+/*
+The "fortune" function below receives both channels as parameters. The arrows show that channel1
+will be read from only in this function and channel2 will only have data sent to it in this function.
+*/
+func fortune(channel1 <-chan int, channel2 chan<- int) {
+	contentInASlice := strings.Split(contentInAString, "%%") //Creates a slice of strings separated by the "%%" symbols. This separates each fortune.
+	for {                                                    //infinite loop
+		numberOfFortunes := len(contentInASlice) //Determines how many strings are in this slice, that is, the number of fortunes in the "fortunes.txt" file
+		/*
+			The line below generates a random number from 0 up to (but NOT including) the number of fortunes in the
+			"fortunes.txt" file. This is important because slices are zero indexed, so we don't want to exceed the number
+			of items in the slice. According to https://pkg.go.dev/math/rand#Rand.Intn, "Intn returns, as an int, a non-negative pseudo-random number in the
+			half-open interval [0,n). It panics if n <= 0."
+		*/
+		randomnumber := rand.Intn(numberOfFortunes)
+		message := <-channel1 //Wait until the channel is loaded with data and once it is, store it in the variable "message"
+		/*
+			The value will only be 1 when the user has entered "yes", and since the value from a channel can only be
+			read once, the user will have to enter "yes" every time they want a new fortune.
+		*/
+		if message == 1 {
+			fmt.Println(contentInASlice[randomnumber]) //Print the fortune with the random number as its index. This picks a random fortune every time.
+			channel2 <- 1                              //Send to channel2 the value of 1. This only happens after the fortune is printed out.
+		}
+	}
 }
